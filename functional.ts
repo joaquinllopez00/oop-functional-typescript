e248ea6ea95789a8ded1e908c8f8b1411a1c47e2
joaquinllopez00 <joaquinllopezzz@gmail.com>
2021-08-23 15:25:43 -0400

    simple run through

---
+++ functional.ts
@@ -0,0 +1,17 @@
+let num = 123;
+
+function toString(val: any) {
+  return val.toString();
+}
+
+const str = toString(num);
+
+//immutable data
+
+const data = Object.freeze([1, 2, 3, 4, 5, 6]);
+
+// functions as arguments
+
+const addEmoji = (val: any) => toString(val) + "!";
+
+const emojiData = data.map(addEmoji);
---
+++ objectOrientated.ts
@@ -0,0 +1,66 @@
+class Emoji {
+  private _prev: string;
+
+  constructor(public _icon: string) {}
+
+  get icon() {
+    return this._icon;
+  }
+
+  get prev() {
+    return this._prev;
+  }
+
+  change(val: string) {
+    this._prev = this._icon;
+    this._icon = val;
+  }
+}
+
+const sun = new Emoji("sun");
+
+//static method
+
+class EmojiStatic {
+  static addOneTo(val) {
+    return 1 + val;
+  }
+}
+
+class Human {
+  constructor(public name: string) {}
+
+  sayHi() {
+    return `hello, ${this.name}`;
+  }
+}
+
+const patrick = new Human(`Patrick Mullot`);
+
+console.log(patrick.sayHi());
+
+class SuperHuman extends Human {
+  heroName: string;
+  constructor(name) {
+    super(name);
+    this.heroName = `HERO ${name}`;
+  }
+
+  superpower() {
+    return `${this.heroName} hits jumpers`;
+  }
+}
+
+const steph = new SuperHuman("Steph Curry");
+
+console.log(steph.sayHi());
+
+const hasName = (name) => {
+  return { name };
+};
+
+const canSayHi = (name) => {
+  return {
+    sayHi: () => `Hello, ${name}`,
+  };
+};
